185da0eba6c1952e01a798d82ba328f100e679dc
rachmanlazu <rachmanlazuardi.9a@gmail.com>
2019-04-06 16:41:24 +0700

    direct chat wa

--- app/src/main/java/com/adivaa/rachmanlazuardi/adivaaskincareclinic/HomeActivity.java
+++ app/src/main/java/com/adivaa/rachmanlazuardi/adivaaskincareclinic/HomeActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     private CardView reserve, rekam, konsul, product;
@@ -21,24 +27,77 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         reserve.setOnClickListener(this);
         rekam.setOnClickListener(this);
-        konsul.setOnClickListener(this);
+        konsul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWhatsApp();
+            }
+        });
         product.setOnClickListener(this);
+
+        //judul header
+        setTitle("Home");
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.reservasi:
                 Intent moveIntent = new Intent(HomeActivity.this, AmbilAntrianActivity.class);
                 startActivity(moveIntent);
                 break;
         }
 
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.rekamMedis:
                 Intent moveIntent = new Intent(HomeActivity.this, RekamMedisActivity.class);
                 startActivity(moveIntent);
                 break;
+        }
+
+        switch (v.getId()) {
+            case R.id.produk:
+                Intent moveIntent = new Intent(HomeActivity.this, ProdukActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
+    }
+
+    /**
+     * Open WhatssApp.
+     *
+     * @param context     current Context, like Activity, App, or Service
+     * @param packageName the full package name of the app to open
+     * @return true if likely successful, false if unsuccessful
+     */
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        try {
+            Intent i = manager.getLaunchIntentForPackage(packageName);
+            if (i == null) {
+                return false;
+                //throw new ActivityNotFoundException();
+            }
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            context.startActivity(i);
+            return true;
+        } catch (ActivityNotFoundException e) {
+            return false;
+        }
+    }
+
+    private void openWhatsApp() {
+        String smsNumber = "6289657011491"; //without '+'
+        try {
+            Intent sendIntent = new Intent("android.intent.action.MAIN");
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.setType("text/plain");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Dokter saya mau konsultasi");
+            sendIntent.putExtra("jid", smsNumber + "@s.whatsapp.net");
+            sendIntent.setPackage("com.whatsapp");
+            startActivity(sendIntent);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error\n" + e.toString(), Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/adivaa/rachmanlazuardi/adivaaskincareclinic/ResevasiActivity.java
+++ app/src/main/java/com/adivaa/rachmanlazuardi/adivaaskincareclinic/ResevasiActivity.java
@@ -2,6 +2,7 @@ package com.adivaa.rachmanlazuardi.adivaaskincareclinic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ResevasiActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class ResevasiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservasi);
+
+        assert getSupportActionBar() !=null; //null check
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 }
